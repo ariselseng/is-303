72889c07a88b80255598f983af0cf9ba6a3d57ab
Ari <ari@ari-Desktop.(none)>
2011-09-12 18:57:44 +0200

    Escaped dots in awk when determining dname and added quotes too

--- xall.sh
+++ xall.sh
@@ -1,21 +1,25 @@
 #! /bin/bash
 # author: Ari Selseng
 # version: v1
-# USAGE: xall filename.zip directory_to_extract_to [OPTIONAL]
+# USAGE: xall filename.zip full_directory_structure_to_extract_to [OPTIONAL]
 
-
-# Check if we got a file to extract
-if [ "$1" == "" ];
-	then
-		echo "You have to give a file to extract."
-fi
+# creating a separate function for the logo, so it's easy to change colors and stuff
+logo () { echo -e "$1"; }
+logo ""
+logo "               .__  .__   "
+logo "___  ________  |  | |  |  "
+logo "\  \/  /\__  \ |  | |  |  "
+logo " >    <  / __ \|  |_|  |__"
+logo "/__/\_ \(____  /____/____/"
+logo "      \/     \/ "
+logo ""
 
 # Checks if there is a defined directory to extract to.
 if [ "$2" == "" ];
 	#If not, use filename (without extension)
 	then
 		# gets directoryname without		
-		dname="$(echo "$1"|awk '{gsub(/.zip|.tar.gz|.tar.bz2|.tar.xz|.tar.lzma|.tar|.rar/, ""); print}')"
+		dname="$(echo "$1"|awk '{gsub(/\.zip|\.tar\.gz|\.tar\.bz2|\.tar\.xz|\.tar\.lzma|\.tar|\.rar/, ""); print}')"
 	else
 		dname="$2"	
 fi
@@ -25,7 +29,7 @@ case "$1" in
 
 		mkdir "$dname" -p
 		unzip -qqo "$1" -d "$dname"
-		echo " Your extracted content is now in $dname"
+		echo "Hopefully, your extracted content is now in $dname"
 
 		;;
 			
@@ -33,17 +37,29 @@ case "$1" in
 		
 		mkdir "$dname" -p
 		tar xf "$1" -C "$dname"
-		echo " Your extracted content is now in $dname"
+		echo "Hopefully, your extracted content is now in $dname"
 		;;
 		
 	*.rar)
 		
 		mkdir "$dname" -p
 		unrar x -o+ -r -y "$1" "$dname"
-		echo " Your extracted content is now in $dname"		
+		echo "Hopefully, your extracted content is now in $dname"		
 		;;
 
 	*)
-		echo "Sorry this filetype is not supported."
+
+		# Check if we got a file to extract
+		if [ "$1" == "" ];
+			then
+				echo "You'll have to provide a supported file to do something."
+		fi
+
+		# Here we check if
+		if [ "$1" != "" ];
+			then
+				echo "Please provide a supported file to extract."
+		fi
+
 		;;
 esac;
