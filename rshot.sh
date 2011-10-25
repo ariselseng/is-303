539bd9c03c1d1eca4f75bf2175a89e33b9c335bb
ari <ari@aribuntu.(none)>
2011-10-25 14:39:30 +0200

    Whole new method to options in rshot.

--- rshot.sh
+++ rshot.sh
@@ -1,43 +1,36 @@
 #! /bin/bash
 
 # author: Ari Selseng
-# version: v1, 15. september 2011
+# version: v1.01, 15. september 2011
 # license: GPLv2
 
+. rshot-config
 
-# The folder to backup locally ex. /home/$USER
-foldertobackup=
-
-# the user to use with ssh ex. john
-remoteuser=
-
-# address to remoteserver (can be host name, dns name or ip) ex. myradserver.org
-backupserver=
-
-# The folder where the backups will be placed. ex. "/home/$remoteuser/Backups/"
-backupserverfolder=""
-
+mkdir -p $tmpfolder
+ln -s "$foldertobackup" "$tmpfolder"/
+ln -s "$foldertobackup2" "$tmpfolder"/	
+foldertobackup=$tmpfolder
 
 # getting the date and time.
 date=`date "+%Y-%m-%dT%H_%M_%S"`
 
-rsync -azP \
+rsync -azPke "ssh -c $sshencryption -i $sshid -p $sshport" \
   --delete \
   --delete-excluded \
   --exclude 'reallyhugefolder' \
-  --link-dest=../current \
-  $foldertobackup $remoteuser@$backupserver:$backupserverfolder/incomplete_backup-$date \
-  && ssh $remoteuser@$backupserver \
-  "mv $backupserverfolder/incomplete_backup-$date $backupserverfolder/backup-$date \
-  && rm -f $backupserverfolder/current \
-  && ln -s backup-$date $backupserverfolder/current"
+  --link-dest=../latest \
+  $foldertobackup/ $remoteuser@$remoteserver:$remoteserverfolder/incomplete_backup-$date \
+  && ssh -c $sshencryption -i $sshid -p $sshport $remoteuser@$remoteserver \
+  "mv $remoteserverfolder/incomplete_backup-$date $remoteserverfolder/backup-$date \
+  && rm -f $remoteserverfolder/latest \
+  && ln -s backup-$date $remoteserverfolder/latest"  
   
-  
+rm -rf "$foldertobackup"
 
 # Commentary on the rsync command:
 #rsync -azP \
 
-# this option makes sure deletes files on client doesn't show up in current backup.
+# this option makes sure deletes files on client doesn't show up in latest backup.
 #  --delete \
 
 # same as above but deletes excluded too.
@@ -47,19 +40,19 @@ rsync -azP \
 #  --exclude-from=exclude.txt \ 
 
 # Tells rsync on the remote server to use the previous backup and links all the files to the new.
-#  --link-dest=../current \ 
+#  --link-dest=../latest \ 
 
 # gives rsync local folder + a temporary remote folder
-#  $foldertobackup $remoteuser@$backupserver:$backupserverfolder/incomplete_backup-$date \ 
+#  $foldertobackup $remoteuser@$remoteserver:$remoteserverfolder/incomplete_backup-$date \ 
 
 # connects  through ssh
-#  && ssh $remoteuser@$backupserver \ 
+#  && ssh $remoteuser@$remoteserver \ 
 
 # moves the temporary folder to a more final folder name
-#  "mv $backupserverfolder/incomplete_backup-$date $backupserverfolder/backup-$date \ 
+#  "mv $remoteserverfolder/incomplete_backup-$date $remoteserverfolder/backup-$date \ 
 
-# Removes the previous "current" (which is just a symlink anyway)
-#  && rm -f $backupserverfolder/current \ 
+# Removes the previous "latest" (which is just a symlink anyway)
+#  && rm -f $remoteserverfolder/latest \ 
 
-# makes the latest backup symlinked to  a new "current"
-#  && ln -s backup-$date $backupserverfolder/current" 
+# makes the latest backup symlinked to  a new "latest"
+#  && ln -s backup-$date $remoteserverfolder/latest" 
